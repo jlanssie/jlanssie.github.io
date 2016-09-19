17399967e40ec18c29f8c207f793b1f8883b3baf
Jeremy Lanssiers <jlanssie@gmail.com>
2016-09-19 02:57:10 +0200

    Update 18/9

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 ------------------------------------------------------ */
 
 function fade($ele) {
-    $ele.fadeIn(1000).delay(3000).fadeOut(1000, function() {
+    $ele.fadeIn(1000).delay(4000).fadeOut(1000, function() {
         var $next = $(this).next('.quote');
         fade($next.length > 0 ? $next : $(this).parent().children().first());
    });
