c3a0a0f0e7a8c8e90547ba00fdc85c5e841c9238
jlanssie <jlanssie@gmail.com>
2017-06-11 01:48:40 +0200

    update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,5 @@
+/* Navbar toggle on mobile devices */
+
 function menuCollapse() {
     var x = document.getElementById("navbar");
     if (x.className === "notClicked") {
@@ -5,5 +7,11 @@ function menuCollapse() {
     } else {
         x.className = "notClicked";
     }
-}
+};
 
+/* Copyright notice automatically updated to the current year */
+
+$(document).ready(function () {
+        var a = (new Date().getFullYear()>2010) ? new Date().getFullYear() : "2010"
+        $("div.copyright").html("&copy; "+ a + " Jeremy Lanssiers");
+});
