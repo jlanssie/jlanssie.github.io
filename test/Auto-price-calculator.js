03aee0d8ae8974106dfd00d7a805f19997de01e0
jlanssie <jlanssie@gmail.com>
2018-09-10 22:35:44 +0200

    test script autocalc

---
+++ test/Auto-price-calculator.js
@@ -0,0 +1,30 @@
+var leasePriceString = prompt("Input lease price per month"); 
+var ecoScoreString = prompt("Input eco score");
+var catalogValueString = prompt("Input catalog value");
+var c02String = prompt("Input C02");
+
+var leasePrice = parseInt(leasePriceString); 
+var ecoScore = parseInt(ecoScoreString);
+var catalogValue = parseInt(catalogValueString);
+var c02 = parseInt(c02String);
+
+var brutoMonth = true;
+var VAA = true;
+
+function calculator(){
+	function brutoMonthCalc(){
+		if (ecoScore >= 74){
+			var brutoMonth = ((leasePrice*12)/13.92)-75;
+			console.log(brutoMonth);
+		} else {
+			var brutoMonth = ((leaseprice*12)/13.92)-150;
+			console.log(brutoMonth);
+		}
+	}
+	function VAACalc(){
+		var VAA =  6/7*catalogValue*(0.055+((c02-105)*0.001));
+		console.log(VAA);
+	}
+};
+
+console.log("Brutoprijs per maand =" + brutoMonth + VAA);
